85988a194d1699614ecdc6f78ff36deb8a105209
o4codes <oforkansi.shadrach@gmail.com>
2023-09-27 07:31:17 +0100

    added dev web entrypoint script

---
+++ .docker/dev_web_entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate --noinput
+python manage.py runserver 0.0.0.0:${BACKEND_PORT}
